1f904f10706004c8bf4ff7dbd54c5aee1bca8d13
MateoMigone <mateomigone@gmail.com>
2024-02-19 18:23:08 -0300

    adding login page

---
+++ layouts/loginlayout.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="loginBackground">
+    <slot />
+  </div>
+  <footer class="loginFooter text-white py-4">
+    <select
+      id="language"
+      name="language"
+      autocomplete="language-name"
+      class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+    >
+      <option value="por">Português</option>
+      <option value="esp">Español</option>
+      <option value="eng">English</option>
+    </select>
+    <a class="text-center">Termos e Condições</a>
+    <a class="text-center">Política de Privacidade</a>
+    <div class="social-media">
+      <svg
+        class="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <path
+          d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+        />
+      </svg>
+      <svg
+        class="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <path
+          d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+        />
+      </svg>
+    </div>
+  </footer>
+</template>
+
+<style>
+.loginBackground {
+  background-image: url("/background.png");
+  background-size: cover;
+  background-position: center;
+  height: 90vh;
+  max-width: 100vw;
+}
+
+.loginFooter {
+  height: 10vh;
+  background-color: #2d5893;
+  display: flex;
+  justify-content: center;
+  gap: 40px;
+  align-items: center;
+}
+
+.social-media {
+  display: flex;
+  gap: 10px;
+}
+
+.icon {
+  width: 17.5px;
+  fill: white;
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,85 @@
 <template>
-  <div>This is a new branch</div>
+  <div
+    class="background flex min-h-full flex-col justify-center px-6 py-12 lg:px-8"
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2
+        class="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+      >
+        Login
+      </h2>
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST">
+        <div class="mt-2">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            autocomplete="email"
+            required
+            placeholder="Seu email..."
+            class="block w-full rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+          />
+        </div>
+
+        <div class="mt-2">
+          <input
+            id="password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+            required
+            placeholder="Sua senha..."
+            class="block w-full rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+          />
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+          >
+            Continuar
+          </button>
+        </div>
+      </form>
+
+      <p class="underline mt-7 text-center text-sm">
+        <NuxtLink to="/">Esqueci minha senha</NuxtLink>
+      </p>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+definePageMeta({
+  layout: "loginlayout",
+});
+</script>
 
-<style scoped></style>
+<style scoped>
+.background {
+  background-image: url("/ortesys-background-logo.png");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+input {
+  background-color: rgba(92, 106, 196, 0.1);
+}
+
+button {
+  background-color: rgba(45, 88, 147, 1);
+}
+
+button:hover {
+  background-color: #2fb7a0;
+}
+
+p {
+  color: rgba(45, 88, 147, 1);
+}
+</style>
