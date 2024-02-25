13159ac34331f81ebe23ab3d15b8429a9a9ab32f
Manuel Diaz Usandivaras <manueldiazusandi2008@gmail.com>
2024-02-25 16:35:36 -0300

    Information card initial structure

---
+++ components/InfoCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div
+      class="bg-white rounded-[13.84px] shadow-lg w-[213.61px] h-[166.05px] flex flex-col items-center justify-center space-y-[6.92px]"
+    >
+      <!-- Apply the custom text color using inline styles -->
+      <span
+        class="text-[43px] font-medium leading-none w-[50px] h-[43px]"
+        style="color: #2fb7a0"
+        >32</span
+      >
+      <span
+        class="text-[29px] font-bold leading-none w-[117.62px] h-[58px]"
+        style="color: #2d5893"
+        >Órteses geradas</span
+      >
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
+import type InfoCardVue from '~/components/InfoCard.vue';
 <template>
   <div>Setting the project</div>
   <OrtesysCard />
+  <InfoCard />
 </template>
