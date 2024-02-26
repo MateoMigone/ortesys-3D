33b62e3058a30c2ffb05e1b717da0d4c1e3d3310
MateoMigone <mateomigone@gmail.com>
2024-02-26 18:02:26 -0300

    adding changes to InfoCard

--- components/InfoCard.vue
+++ components/InfoCard.vue
@@ -1,19 +1,14 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <div
-      class="bg-white rounded-[13.84px] shadow-lg w-[213.61px] h-[166.05px] flex flex-col items-center justify-center space-y-[6.92px]"
+  <div
+    class="bg-white rounded-2xl drop-shadow w-[213.61px] h-[166.05px] flex flex-col items-center justify-center gap-[6.92px]"
+  >
+    <span
+      class="text-[43px] font-medium leading-none w-[50px] h-[43px] text-[#2fb7a0]"
+      >32</span
     >
-      <!-- Apply the custom text color using inline styles -->
-      <span
-        class="text-[43px] font-medium leading-none w-[50px] h-[43px]"
-        style="color: #2fb7a0"
-        >32</span
-      >
-      <span
-        class="text-[29px] font-bold leading-none w-[117.62px] h-[58px]"
-        style="color: #2d5893"
-        >Órteses geradas</span
-      >
-    </div>
+    <span
+      class="text-[29px] font-bold leading-none w-[117.62px] h-[58px] text-[#2d5893]"
+      >Órteses geradas</span
+    >
   </div>
 </template>
