64fe8c639dea7db065597db76311acbabc0b9309
MateoMigone <mateomigone@gmail.com>
2024-03-04 10:48:02 -0300

    working on main-layout

--- layouts/main-layout.vue
+++ layouts/main-layout.vue
@@ -197,79 +197,104 @@
             >
           </li>
         </ul>
-        <div class="flex justify-center items-center gap-2 mt-4">
-          <svg
-            width="15"
-            height="14"
-            viewBox="0 0 15 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.96625 0H6.4107V7.77778H7.96625V0ZM11.7229 1.68778L10.6185 2.79222C11.8474 3.78 12.6329 5.29667 12.6329 7C12.6329 10.01 10.1985 12.4444 7.18848 12.4444C4.17848 12.4444 1.74403 10.01 1.74403 7C1.74403 5.29667 2.52959 3.78 3.7507 2.78444L2.65403 1.68778C1.14514 2.97111 0.188477 4.86889 0.188477 7C0.188477 10.8656 3.32292 14 7.18848 14C11.054 14 14.1885 10.8656 14.1885 7C14.1885 4.86889 13.2318 2.97111 11.7229 1.68778Z"
-              fill="#2D5893"
-            />
-          </svg>
-          <span class="text-[#2d5893]">Sair</span>
+        <div class="mt-4 flex justify-center">
+          <NuxtLink to="/login" class="flex items-center gap-2 w-min">
+            <svg
+              width="15"
+              height="14"
+              viewBox="0 0 15 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.96625 0H6.4107V7.77778H7.96625V0ZM11.7229 1.68778L10.6185 2.79222C11.8474 3.78 12.6329 5.29667 12.6329 7C12.6329 10.01 10.1985 12.4444 7.18848 12.4444C4.17848 12.4444 1.74403 10.01 1.74403 7C1.74403 5.29667 2.52959 3.78 3.7507 2.78444L2.65403 1.68778C1.14514 2.97111 0.188477 4.86889 0.188477 7C0.188477 10.8656 3.32292 14 7.18848 14C11.054 14 14.1885 10.8656 14.1885 7C14.1885 4.86889 13.2318 2.97111 11.7229 1.68778Z"
+                fill="#2D5893"
+              />
+            </svg>
+            <span class="text-[#2d5893]">Sair</span>
+          </NuxtLink>
         </div>
       </div>
     </div>
 
     <!-- Main Content -->
-    <div class="flex-1 flex flex-col overflow-hidden">
+    <div class="flex flex-col p-5 w-2/3">
       <!-- Header -->
-      <header class="flex justify-between items-center p-5 border-b-2">
-        <div class="flex space-x-2">
-          <!-- Search input -->
+      <header
+        class="sticky top-5 bg-white py-4 px-8 w-[90%] mx-auto rounded-2xl flex justify-between gap-3"
+      >
+        <!-- Search bar -->
+        <div>
           <input
-            type="search"
+            type="text"
+            class="pl-10 pr-4 py-2 rounded-full shadow-sm bg-[#f4f4f4] placeholder:text-sm text-sm"
             placeholder="Pesquisar"
-            class="px-4 py-2 w-80 focus:ring-blue-500 focus:border-blue-500"
           />
-          <button class="text-blue-500">
-            <i class="fas fa-search"></i>
-          </button>
         </div>
-        <div class="flex space-x-4">
-          <!-- Icons -->
-          <button class="text-blue-500">
-            <i class="far fa-bell"></i>
-          </button>
-          <button class="text-blue-500">
-            <i class="far fa-envelope"></i>
-          </button>
+
+        <!-- Menu items -->
+        <div class="flex items-center gap-4">
+          <button class="text-[#2d5893] font-semibold">Membro Superior</button>
+          <button class="text-[#2d5893] font-semibold">Membro Inferior</button>
+        </div>
+        <div class="flex items-center gap-2">
+          <div
+            class="bg-[#2d5893] rounded-md w-[34px] h-[34px] flex items-center justify-center"
+          >
+            <svg
+              width="15"
+              height="18"
+              viewBox="0 0 14 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M1.73665 1.75V14.25L6.11911 10.071C6.60126 9.61125 7.35893 9.61125 7.84109 10.071L12.2235 14.25V1.75H1.73665ZM0.238525 1.5C0.238525 0.809644 0.797471 0.25 1.48697 0.25H12.4732C13.1627 0.25 13.7217 0.809645 13.7217 1.5V14.8333C13.7217 15.9323 12.4071 16.4964 11.6122 15.7385L6.9801 11.3214L2.34796 15.7385C1.55313 16.4964 0.238525 15.9323 0.238525 14.8333V1.5Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+
+          <div
+            class="bg-[#2d5893] rounded-md w-[34px] h-[34px] flex items-center justify-center"
+          >
+            <svg
+              width="15"
+              height="17"
+              viewBox="0 0 12 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.42453 15C5.51267 15 4.77659 14.2269 4.77659 13.2692H8.07247C8.07247 14.2269 7.33639 15 6.42453 15ZM11.9177 12.1154H0.931396V10.9615L2.03002 9.80769V6.79615C2.03002 4.35 3.16161 2.27308 5.3259 1.73077V1.15385C5.3259 0.496154 5.79831 0 6.42453 0C7.05075 0 7.52316 0.496154 7.52316 1.15385V1.73077C9.68745 2.27308 10.819 4.36154 10.819 6.79615V9.80769L11.9177 10.9615V12.1154Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+
+          <div
+            class="bg-[#2d5893] rounded-md w-[34px] h-[34px] flex items-center justify-center"
+          >
+            <svg
+              width="21"
+              height="23"
+              viewBox="0 0 16 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.24595 11.3333H8.66085V9.91667H7.24595V11.3333ZM7.9534 0C4.04828 0 0.878906 3.17333 0.878906 7.08333C0.878906 10.9933 4.04828 14.1667 7.9534 14.1667C11.8585 14.1667 15.0279 10.9933 15.0279 7.08333C15.0279 3.17333 11.8585 0 7.9534 0ZM7.9534 12.75C4.83355 12.75 2.2938 10.2071 2.2938 7.08333C2.2938 3.95958 4.83355 1.41667 7.9534 1.41667C11.0732 1.41667 13.613 3.95958 13.613 7.08333C13.613 10.2071 11.0732 12.75 7.9534 12.75ZM7.9534 2.83333C6.38993 2.83333 5.1236 4.10125 5.1236 5.66667H6.5385C6.5385 4.8875 7.1752 4.25 7.9534 4.25C8.73159 4.25 9.36829 4.8875 9.36829 5.66667C9.36829 7.08333 7.24595 6.90625 7.24595 9.20833H8.66085C8.66085 7.61458 10.7832 7.4375 10.7832 5.66667C10.7832 4.10125 9.51686 2.83333 7.9534 2.83333Z"
+                fill="white"
+              />
+            </svg>
+          </div>
         </div>
       </header>
 
       <!-- Main section -->
-      <main class="flex-1 overflow-y-auto p-5">
-        <div class="bg-white p-8 rounded-md w-full">
-          <div class="flex items-center justify-between pb-6">
-            <div>
-              <h2 class="text-gray-600 font-semibold">
-                Selecione o tipo de órtese desejado.
-              </h2>
-              <span class="text-xs">Todos os campos são obrigatórios.</span>
-            </div>
-            <div class="flex items-center justify-between">
-              <div class="lg:ml-40 ml-10 space-x-8">
-                <button
-                  class="btn border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition ease-out duration-500"
-                >
-                  Membro Superior
-                </button>
-                <button
-                  class="btn border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition ease-out duration-500"
-                >
-                  Membro Inferior
-                </button>
-              </div>
-            </div>
-          </div>
-          <slot />
-          <!-- Content here -->
-        </div>
-      </main>
+      <slot />
 
       <!-- Footer -->
       <footer class="flex justify-between items-center p-5 border-t">
