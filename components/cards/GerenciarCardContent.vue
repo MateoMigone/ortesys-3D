707ca567a677d02749a2c32bd8e5fcaac6e932d9
Manuel Diaz Usandivaras <manueldiazusandi2008@gmail.com>
2024-03-10 18:07:24 -0300

    Gerenciar card intitial structure

---
+++ components/cards/GerenciarCardContent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="flex items-center justify-center w-[800px] h-[132px] border-2 border-gray-200 bg-white"
+  >
+    <div class="flex items-center w-full px-4 gap-[55px]">
+      <div class="flex flex-col items-center gap-[16px]">
+        <h4 class="font-[700] text-[20px] leading-[20px] text-[#2D5893]">
+          Rigidez
+        </h4>
+        <div
+          class="flex justify-center items-center w-[87px] h-[40px] rounded-md border-[1px] bg-[#FCFCFF] border-[#CDCEE4]"
+        >
+          <span class="font-[500] text-[14px] leading-[14px]">1 a 3</span>
+        </div>
+      </div>
+      <div class="flex flex-col items-center gap-[16px]">
+        <h4 class="font-[700] text-[20px] leading-[20px] text-[#2D5893]">
+          Calcanhar
+        </h4>
+        <div
+          class="flex justify-center items-center w-[121px] h-[40px] rounded-md border-[1px] bg-[#FCFCFF] border-[#CDCEE4]"
+        >
+          <span class="font-[500] text-[14px] leading-[14px]">16x21cm</span>
+        </div>
+      </div>
+      <div class="flex flex-col items-center gap-[16px]">
+        <h4 class="font-[700] text-[20px] leading-[20px] text-[#2D5893]">
+          Largura
+        </h4>
+        <div
+          class="flex justify-center items-center w-[93px] h-[40px] rounded-md border-[1px] bg-[#FCFCFF] border-[#CDCEE4]"
+        >
+          <span class="font-[500] text-[14px] leading-[14px]">16x21cm</span>
+        </div>
+      </div>
+      <div class="flex flex-col items-center gap-[16px]">
+        <h4 class="font-[700] text-[20px] leading-[20px] text-[#2D5893]">
+          Comprimento
+        </h4>
+        <div
+          class="flex justify-center items-center w-[162px] h-[40px] rounded-md border-[1px] bg-[#FCFCFF] border-[#CDCEE4]"
+        >
+          <span class="font-[500] text-[14px] leading-[14px]">16x21cm</span>
+        </div>
+      </div>
+      <div class="ml-auto">
+        <Button text="Tutorial" color="default" />
+      </div>
+    </div>
+  </div>
+</template>
