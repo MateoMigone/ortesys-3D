565b3adaf070283a4de8f39f3ffc87c19385e68a
MateoMigone <mateomigone@gmail.com>
2024-03-12 13:21:43 -0300

    creating myAccount page initial structure

---
+++ components/cards/StatCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="flex flex-col items-center text-center w-full gap-3 shadow-sm rounded-xl py-8 2xl:py-10"
+  >
+    <span class="text-[33px] font-medium text-[#2fb7a0] leading-8 2xl:text-4xl">
+      {{ value }}
+    </span>
+    <p
+      class="font-bold text-[#2d5893] text-center text-2xl leading-6 max-w-[120px] 2xl:leading-7 2xl:text-[27px] 2xl:max-w-[130px]"
+    >
+      {{ title }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { title, value } = defineProps(["title", "value"]);
+</script>
--- pages/myAccount.vue
+++ pages/myAccount.vue
@@ -1,5 +1,82 @@
 <template>
-  <div>This will be "my account" page</div>
+  <div
+    class="w-[90%] mx-auto bg-white shadow-lg rounded-2xl p-7 flex flex-col gap-4"
+  >
+    <div class="rounded-2xl border-[1px] p-5 flex justify-center">
+      <form
+        action=""
+        class="flex flex-col items-center justify-center gap-6 w-2/3"
+      >
+        <div class="flex flex-col items-center gap-2 2xl:mt-5">
+          <img
+            class="h-[62px] w-[62px] rounded-full border-2 border-[#2d5893] p-1"
+            src="../assets/imgs/user-avatar.png"
+            alt="Profile image"
+          />
+          <h4 class="font-semibold text-[#2d5893] text-lg">Dr Paulo Tavares</h4>
+        </div>
+
+        <div class="w-full max-w-[475px] flex flex-col gap-4">
+          <div>
+            <label for="fist" class="text-[#2d5893] font-bold text-xl"
+              >Nome</label
+            ><br />
+            <input
+              type="text"
+              name="fist"
+              id="fist"
+              class="bg-[#FCFCFF] border-[#CDCEE4] border-[1px] rounded-lg my-1.5 w-full py-3 px-4 placeholder:text-[#131E41] font-medium text-xs 2xl:text-sm"
+              placeholder="Digite seu nome completo"
+            />
+          </div>
+          <div>
+            <label for="fist" class="text-[#2d5893] font-bold text-xl"
+              >E-mail</label
+            ><br />
+            <input
+              type="text"
+              name="fist"
+              id="fist"
+              class="bg-[#FCFCFF] border-[#CDCEE4] border-[1px] rounded-lg my-1.5 w-full py-3 px-4 placeholder:text-[#131E41] font-medium text-xs 2xl:text-sm"
+              placeholder="cliente@gmail.com"
+            />
+          </div>
+          <div>
+            <label for="fist" class="text-[#2d5893] font-bold text-xl"
+              >Telefone*</label
+            ><br />
+            <input
+              type="text"
+              name="fist"
+              id="fist"
+              class="bg-[#FCFCFF] border-[#CDCEE4] border-[1px] rounded-lg my-1.5 w-full py-3 px-4 placeholder:text-[#131E41] font-medium text-xs 2xl:text-sm"
+              placeholder="+55 (DDD) 00000-0000"
+            />
+          </div>
+        </div>
+        <Button
+          color="secondary"
+          text="Salvar alterações"
+          class="w-3/5 max-w-[285px] my-3"
+        />
+      </form>
+    </div>
+    <div
+      class="rounded-2xl border-[1px] p-5 grid grid-cols-3 grid-rows-2 p-5 gap-6 place-items-center"
+    >
+      <StatCard title="Órteses geradas" value="32" />
+
+      <StatCard title="Receita estimada" value="R$32,00" />
+
+      <StatCard title="Gesso salvo" value="32kg" />
+
+      <StatCard title="Filamento utilizado" value="32kg" />
+
+      <StatCard title="Velcro utilizado" value="32m" />
+
+      <StatCard title="Tempo de impressão" value="32h" />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,3 +84,5 @@ definePageMeta({
   layout: "main-layout",
 });
 </script>
+
+<style scoped></style>
