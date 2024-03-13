f29e613806e4e069dfc037bdd8768fc1fe032809
MateoMigone <mateomigone@gmail.com>
2024-03-13 11:59:21 -0300

    adding changes to support page

--- pages/support.vue
+++ pages/support.vue
@@ -1,38 +1,39 @@
 <template>
-  <div class="widith-[90%] mx-auto">
+  <div class="w-[90%] mx-auto">
     <TextBanner
       title="Como podemos ajudar? "
       subtitle="Contacte-nos através dos links abaixo:"
     />
     <div
-      class="flex justify-center items-center h-[553px] rounded-2xl bg-white"
+      class="flex justify-center items-center rounded-2xl bg-white py-12 2xl:py-16"
     >
-      <div class="flex flex-col items-center w-[555px] h-[418px] gap-6">
+      <div class="flex flex-col items-center gap-6">
         <div>
           <Button
             text="Acessar perguntas frequentes"
             color="purple"
-            class="w-64 h-10"
+            class="w-[270px]"
           />
         </div>
-        <div class="flex flex-col items-center w-[556px] h-[105px]">
-          <h3 class="font-bold text-4xl text-[#2D5893]">
+        <div class="flex flex-col items-center">
+          <h3 class="font-bold text-[#2D5893] text-3xl 2xl:text-4xl">
             Ainda está com dúvida?
           </h3>
-          <span class="font-medium text-2xl text-[#2D5893] text-center"
+          <span
+            class="font-medium text-[#2D5893] text-center w-[450px] text-xl 2xl:text-2xl 2xl:w-[500px]"
             >Entre em contato conosco através dos nossos canais de atendimento
             online :)</span
           >
         </div>
-        <div class="flex flex-col items-center w-[251px] h-[76px] gap-4">
+        <div class="flex flex-col items-center gap-3">
           <span class="text-xl font-bold text-[#2D5893]">E-mail</span>
-          <Button text="Enviar um e-mail" color="purple w-[251px] h-[40px]" />
+          <Button text="Enviar um e-mail" color="purple" class="w-[270px]" />
         </div>
-        <div class="flex flex-col items-center w-[251px] h-[76px] gap-4">
+        <div class="flex flex-col items-center gap-3">
           <span class="text-xl font-bold text-[#2D5893]">Chat</span>
-          <Button text="Abrir o chat" color="purple w-[251px] h-[40px]" />
+          <Button text="Abrir o chat" color="purple" class="w-[270px]" />
         </div>
-        <div class="flex justify-between gap-1.5">
+        <div class="flex justify-between gap-2">
           <Button text="Feedback" color="secondary" />
           <Button text="Videos tutoriais" />
           <Button text="Solicitar órteses diferente" color="red" />
