9e18a85ad44788ad2327b5b0ca77b86e24b92377
MateoMigone <mateomigone@gmail.com>
2024-03-13 13:12:47 -0300

    adding first view in printer management

--- components/cards/ConfigCard.vue
+++ components/cards/ConfigCard.vue
@@ -1,16 +1,10 @@
 <template>
-  <div
-    class="w-[800px] h-[114.38px] rounded-xl shadow-sm flex items-center px-6"
-  >
+  <div class="rounded-xl shadow-sm flex items-center bg-white p-10 w-full">
     <h3 class="text-2xl font-bold">{{ text }}</h3>
     <div class="flex gap-2.5 ml-auto">
-      <Button text="Editar" class="w-[165px] h-[40px]" />
-      <Button text="Remover" color="red" class="w-[165px] h-[40px]" />
-      <Button
-        text="Controle de acesso"
-        color="yellow"
-        class="w-[165px] h-[40px]"
-      />
+      <Button text="Editar" class="w-[100px] 2xl:w-[165px]" />
+      <Button text="Remover" color="red" class="w-[100px] 2xl:w-[165px]" />
+      <Button text="Controle de acesso" color="yellow" class="w-[165px]" />
     </div>
   </div>
 </template>
--- pages/managePrinter.vue
+++ pages/managePrinter.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>This will be "manage printer" page</div>
+  <div class="w-[90%] mx-auto flex flex-col items-center gap-6">
+    <div
+      class="bg-white p-8 w-[40%] min-w-[300px] flex justify-center rounded-2xl"
+    >
+      <Button
+        text="Adicionar nova impressora"
+        color="green"
+        class="w-[240px]"
+      />
+    </div>
+    <ConfigCard text="Impressora 1" />
+    <ConfigCard text="Impressora 2" />
+    <ConfigCard text="Impressora 3" />
+    <ConfigCard text="Impressora 4" />
+    <ConfigCard text="Impressora 5" />
+    <ConfigCard text="Impressora 6" />
+    <ConfigCard text="Impressora 7" />
+  </div>
 </template>
 
 <script setup>
